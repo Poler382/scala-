84d8a348e827191172a0b15ff4a2dd30253de185
Yuya Miyaji <36500871+Poler382@users.noreply.github.com>
2018-09-14 17:43:10 +0900

    Group Normalization書いてます
    forward を書いてます。
    でもdropなどを使わないほうがいいみたいです。
    add multiなど配列を渡されたら一括で計算するmap見たいなものを作るといいかも

--- BatchLayer.scala
+++ BatchLayer.scala
@@ -1,6 +1,8 @@
 import kata._
-class BNa(val xn: Int, val eps:T= 0.001:T, val rho1:T = 0.9:T, val rho2:T= 0.999:T) extends Layer {
-  var gamma = new Array[T](xn).map(_ => 1: T)
+class BNa(
+  val xn: Int, val eps:T= 0.001f,
+  val rho1:T = 0.9f, val rho2:T= 0.999f) extends Layer {
+  var gamma = new Array[T](xn).map(_ => 1:T)
   var beta = new Array[T](xn)
   var dgamma = new Array[T](gamma.size)
   var dbeta = new Array[T](beta.size)
@@ -8,10 +10,10 @@ class BNa(val xn: Int, val eps:T= 0.001:T, val rho1:T = 0.9:T, val rho2:T= 0.999
   val adam_beta = new Adam(beta.size, eps, rho1, rho2)
   var xmu = Array.ofDim[T](1, xn) // rhs is just a placeholder value
   var sigma = new Array[T](xn)
-  val delta = 1e-8:T
+  val delta = 1e-8f
   var mmu = new Array[T](xn)
   var msigma = new Array[T](xn)
-  val decay = 0.999:T
+  val decay = 0.999f
 
   def forward(x: Array[T]): Array[T] = {
     val y = new Array[T](xn)
@@ -30,7 +32,7 @@ class BNa(val xn: Int, val eps:T= 0.001:T, val rho1:T = 0.9:T, val rho2:T= 0.999
     val m = xs.size
     xmu = Array.ofDim[T](m, xn)
     for (j <- 0 until xn) {
-      var mu = 0d
+      var mu = 0:T
       for (i <- 0 until m) {
         mu += xs(i)(j)
       }
@@ -38,10 +40,10 @@ class BNa(val xn: Int, val eps:T= 0.001:T, val rho1:T = 0.9:T, val rho2:T= 0.999
       mmu(j) = decay * mmu(j) + (1 - decay) * mu
       for (i <- 0 until m) {
         xmu(i)(j) = xs(i)(j) - mu
-        sigma(j) += xmu(i)(j) * xmu(i)(j)
+        sigma(j) += (xmu(i)(j) * xmu(i)(j)).toFloat
       }
-      sigma(j) = math.sqrt(sigma(j) / m + delta)
-      msigma(j) = decay * msigma(j) + (1 - decay) * sigma(j)
+      sigma(j) = (math.sqrt(sigma(j) / m + delta)).toFloat
+      msigma(j) = (decay * msigma(j) + (1 - decay) * sigma(j)):T
     }
 
     var ys = Array.ofDim[T](m, xn)
@@ -72,13 +74,13 @@ class BNa(val xn: Int, val eps:T= 0.001:T, val rho1:T = 0.9:T, val rho2:T= 0.999
       val d3 = -d2 / (sigma(j) * sigma(j))
       val d4 = d3 / (2 * sigma(j))
 
-      var d8 = 0d
+      var d8 = 0:T
       var d10 = new Array[T](m)
       for (i <- 0 until m) {
         val d5 = d4 / m
         val d6 = 2 * xmu(i)(j) * d5
         val d7 = d1(i) / sigma(j)
-        d10(i) = d6 + d7
+        d10(i) = (d6 + d7).toFloat
         d8 -= d10(i)
       }
       val d9 = d8 / m
@@ -100,4 +102,85 @@ class BNa(val xn: Int, val eps:T= 0.001:T, val rho1:T = 0.9:T, val rho2:T= 0.999
     dgamma = new Array[T](gamma.size)
     dbeta = new Array[T](beta.size)
   }
+}
+
+class GNa(
+  val xn:Int, // 100
+  val gs:Int,//チャンネルを何分割するか
+  val IC:Int = 1,//input chanel
+  val eps:T  = 0.001f,
+  val rho1:T = 0.9f,
+  val rho2:T = 0.999f
+)extends Layer{
+  var gamma = new Array[T](xn)
+  var beta  = new Array[T](xn)
+  val delta = 1e-8f
+  val adam_gamma = new Adam(gamma.size,eps,rho1,rho2)
+  val adam_beta  = new Adam(beta.size,eps,rho1,rho2)
+  
+  def forward(d:Array[T]) : Array[T] = {
+    
+    val y = new Array[T](xn)
+    for (i <- 0 until xn) {
+      val xh = (x(i) - mmu(i)) / (msigma(i) + delta)
+      y(i) = xh * gamma(i) + beta(i)
+    }
+    y
+  }
+
+  def backward(d:Array[T]) : Array[T] = {
+    d
+  }
+
+  override def forward(xs:Array[Array[T]]):Array[Array[T]]={
+    var y = List[Array[T]](xs.size)
+    //まずはチャンネルごとにわける
+    for(x <- xs){
+      var dy = List[Array[T]](xs.size)
+      val xsize=x.size/IC
+      var cdrop:Array[T] = null
+      cdrop = x.clone
+
+      for(cn <- 0 until IC){
+        val cdate = cdrop.take(xsize)//チャンネルを取り出す
+        cdro  = x.drop(xsize)//残りを保存
+        var cy = List[Array[T]](xs.size)
+        var ddrop:Array[T] = null
+        ddrop = cdrop.clone
+        for(i <- 0 until gs){
+          val gndate = ddrop.take(ddrop.size/gs)
+          ddrop=ddrop.drop(ddrop.size/gs)
+          val t_myu   = gndate.sum / gndate.size
+          val t_sigma = gndate.map((_ - t_myu)*(_ - t_myu)).sum / gndate.size
+          val t_xhat  = gndate.map((_-t_myu)/sqrt(t_sigma + delta))
+          val t_y     = gamma.zip(t_xhat).map{case(a,b)=>a*b}.zip(beta).map{case(a,b)=>a+b}
+          cy ::= t_y//group Array
+        }
+        dy ::= cy.toArray.flatten
+      }
+      y ::= dy
+    }
+    y.toArray
+  }
+
+  def backward():Array
+  }
+}
+
+object test{
+  def trial(){
+
+    val xs = Array(
+      Array(1f,2f,3f,4f,5f,6f),
+      Array(2f,3f,4f,5f,6f,7f),
+      Array(3f,4f,5f,6f,7f,8f),
+      Array(5f,6f,7f,8f,9f,10f)
+    )
+
+    val gn = new GNa(3,2,1)
+
+    gn.forward(xs)
+
+  }
+
 }
