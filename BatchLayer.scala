e8508c672182edb9bdd9e5ce868474aa63abf895
Yuya Miyaji <36500871+Poler382@users.noreply.github.com>
2018-09-19 20:06:53 +0900

    backwardのd3まで計算終了した

--- BatchLayer.scala
+++ BatchLayer.scala
@@ -107,40 +107,40 @@ class BNa(
 class GNa(
   val xn:Int,
   val W:Int,
-  val H:Int,2
+  val H:Int,
   val gs:Int,//チャンネルを何分割するか
   val IC:Int = 1,//input chanel
   val eps:T  = 0.001f,
   val rho1:T = 0.9f,
   val rho2:T = 0.999f
 )extends Layer{
-  var gamma = new Array[T](xn,gs)
-  var beta  = new Array[T](xn,gs)
+  var gamma = Array.ofDim[T](xn,IC,gs).map(_.map(_.map(_=> 1f)))
+  var beta  = Array.ofDim[T](xn,IC,gs)
   val delta = 1e-8f
   val adam_gamma = new Adam(gamma.size,eps,rho1,rho2)
   val adam_beta  = new Adam(beta.size,eps,rho1,rho2)
+  var dgamma = Array.ofDim[T](xn,IC,gs)
+  var dbeta  = Array.ofDim[T](xn,IC,gs)
+ 
   
   def forward(d:Array[T]) : Array[T] = {
-    
-    val y = new Array[T](xn)
-    for (i <- 0 until xn) {
-      val xh = (x(i) - mmu(i)) / (msigma(i) + delta)
-      y(i) = xh * gamma(i) + beta(i)
-    }
-    y
+    println("nowonownow")
+    d
   }
 
   def backward(d:Array[T]) : Array[T] = {
     d
   }
 
+  var t_myu   = Array.ofDim[T](xn,IC,gs)
+  var t_sigma = Array.ofDim[T](xn,IC,gs)
+  var t_xhat  = Array.ofDim[T](xn,W*H*IC)
+
   override def forward(xs:Array[Array[T]]):Array[Array[T]]={
-    var y       = List[Array[T]]()
-    var t_myu   = Array.ofDim[T](gs)
-    var t_sigma = Array.ofDim[T](gs)
-    var t_xhat  = Array.ofDim[T](W*H*IC)
+    var y       = Array.ofDim[T](xs.size,xs(0).size)
     
-    for(x <- xs){
+    for(k <- 0 until xs.size){
+      val x = xs(k).clone
       for(ch<- 0 until IC){
         val head = ch * W * H
         for(j<-0 until gs){
@@ -149,48 +149,102 @@ class GNa(
           for(i <- 0 until W*H/gs){
             sum += x(head+ghead+i)
           }
-          t_myu(j) = sum / W*H/gs
+          t_myu(k)(ch)(j) = sum / W*H/gs
 
           sum = 0f
           for(i <- 0 until W*H/gs){
-            sum += (x(head+ghead+i)-t_myu(j)) * (x(head+ghead+i)-t_myu(j))
+            sum += (x(head+ghead+i)-t_myu(k)(ch)(j))*(x(head+ghead+i)-t_myu(k)(ch)(j))
           }
 
-          t_sigma(j) = sqrt(sum/(W*H/gs)+eps)
+          t_sigma(k)(ch)(j) = math.sqrt(sum/(W*H/gs)+eps).toFloat
 
           for(i <- 0 until W*H/gs){
-            t_xhat(head+ghead+i) = (x(head+ghead+i)-t_myu(j))/t_sigma(j)
+            t_xhat(k)(head+ghead+i)=(x(head+ghead+i)-t_myu(k)(ch)(j))/t_sigma(k)(ch)(j)
+            
           }
+        }
+      }
+
+      for(ch <- 0 until IC;j <- 0 until gs;i <- 0 until W*H/gs){
+        val head = ch * W * H
+        val ghead = W*H/gs * j
+        y(k)(head+ghead+i) = gamma(k)(ch)(j)*t_xhat(k)(head+ghead+i)+beta(k)(ch)(j)
+      }
+    }
+    y.toArray
+  }
+
+  override def backward(ds:Array[Array[T]]):Array[Array[T]]={
+    var dx = Array.ofDim[T](ds.size,ds(0).size)
+    var d2 = Array.ofDim[T](xn,ds(0).size)
+    var d3 = Array.ofDim[T](xn,ds(0).size)
+    var d4 = Array.ofDim[T](xn,ds(0).size)
+    
+    for(i<- 0 until ds.size){
+      for(ch <- 0 until IC){
+        val head = ch * W * H
+        for(j<-0 until gs){
+          val ghead = W*H/gs * j
+          for(k<- 0 until W*H/gs){
+            dbeta(i)(ch)(j)     += ds(i)(head+ghead+k)
+            dgamma(i)(ch)(j)    += ds(i)(head+ghead+k)* t_xhat(k)(head+ghead+k)
+            d2(i)(head+ghead+k) += gamma(i)(ch)(j)*ds(i)(head+ghead+k)
+            d3(i)(head+ghead+k) += d2(i)(head+ghead+k)/t_sigma(i)(ch)(j)
+            d4(i)(head+ghead+k) += t_myu(i)(ch)(j) * d2(i)(head+ghead+k)
+          }
+
+
+
+
+          
+
+
+
 
         }
 
+
+
+
+
       }
-      for(){
-        //最後 γ、βの計算を考える
-        //上のγ、βの形を考える
-      }
+
     }
+
+
+
+
+  }
+  def update() {
+    /*adam_beta.update(beta, dbeta)
+    adam_gamma.update(gamma, dgamma)
+    reset()*/
   }
 
-  override def backward(d:Array[Array[T]]):Array[Array[T]]={//*
+  def reset() {
+    dgamma = Array.ofDim[T](xn,IC,gs)
+    dbeta  = Array.ofDim[T](xn,IC,gs)
+  }
 
-  }//*
 }
 
 object test{
-  def trial(){
-
+  def trial()={
+ /*   val xn:Int,
+  val W:Int,
+  val H:Int,
+  val gs:Int,//チャンネルを何分割するか
+  */
     val xs = Array(
-      Array(1f,2f,3f,4f,5f,6f),
-      Array(2f,3f,4f,5f,6f,7f),
-      Array(3f,4f,5f,6f,7f,8f),
-      Array(5f,6f,7f,8f,9f,10f)
+      Array(1f,2f,3f,4f),
+      Array(2f,3f,4f,5f),
+      Array(3f,4f,5f,6f),
+      Array(5f,6f,7f,8f)
     )
 
-    val gn = new GNa(3,2,1)
+    val gn = new GNa(4,2,2,2)
 
     gn.forward(xs)
-
   }
 
 }
